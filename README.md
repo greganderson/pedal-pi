89f8a0255df3aceabea509a1c7a4bd9cff42c8a9
Greg Anderson <greg.anderson.cs@gmail.com>
2021-05-31 13:15:47 -0400

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pedal-pi
-Raspberry Pi script that broadcasts strings
+
+Raspberry Pi script that broadcasts strings when a button is pushed.
+
+## Setup
+
+This script assumes that pin 5 is ground and pin 6 is GPIO.  To run:
+
+```bash
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from socket import *
+
+
+GPIO.setmode(GPIO.BCM)
+
+button = 23
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+last_state = 1
+current_state = 0
+
+next_page = 'Next page :)'.encode('UTF-8')
+previous_page = 'Previous page :)'.encode('UTF-8')
+
+ip_address = '192.168.1.255'
+port = 9050
+
+cs = socket(AF_INET, SOCK_DGRAM)
+cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+# TODO: Add previous page button
+try:
+	print('Getting ready to turn some pages!')
+	while True:
+		current_state = GPIO.input(button)
+		if last_state == GPIO.LOW and current_state == GPIO.HIGH:
+			print('Sending a message to turn the page...')
+			cs.sendto(next_page, (ip_address, port))
+			#cs.sendto(previous_page, (ip_address, port))
+			sleep(1)
+		last_state = current_state
+except KeyboardInterrupt:
+	print('Ending my joyous journey of page turning.')
+	exit(0)
